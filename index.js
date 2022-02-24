4aa36014fbbeb138c228d7227448185b557918cb
Saturno Mangieri <saturnob612@gmail.com>
2022-02-24 08:37:56 -0400

    adding cors

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const { request, response } = require('express');
 const express = require('express');
-//const cors = require('cors');
+const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 3000;
 var db = require('./src/Config/db');
 const config = require('./src/Config/config');
 
 //use cors
-//app.use(cors());
+app.use(cors());
 
 var auth_route = require('./src/Routes/auth_route');
 app.use(config.authEP, auth_route);
