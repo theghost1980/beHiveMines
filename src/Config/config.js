8e93392187646736d79ad132826cf76fff95bd70
Saturno Mangieri <saturnob612@gmail.com>
2022-03-07 11:36:20 -0400

    fixes

--- src/Config/config.js
+++ src/Config/config.js
@@ -3,5 +3,5 @@ require('dotenv').config();
 module.exports = {
     'authEP': process.env.PRE_FIX_ALL_ROUTES + process.env.AUTH_ROUTE,
     'charEP': process.env.PRE_FIX_ALL_ROUTES + process.env.CHAR_ROUTE,
-    'userCharEP': process.PRE_FIX_ALL_ROUTES + process.env.USER_CHAR_ROUTE,
+    'userCharEP': process.env.PRE_FIX_ALL_ROUTES + process.env.USER_CHAR_ROUTE,
 };
