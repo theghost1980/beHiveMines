5ad4d09b78e2446daa2730a860183079c86440bd
Saturno Mangieri <saturnob612@gmail.com>
2022-03-08 09:46:58 -0400

    fixes on game_item

--- src/Routes/game_item.js
+++ src/Routes/game_item.js
@@ -9,23 +9,28 @@ router.use(bodyParser.json());
 //end config
 
 router.post('/search', function(req,res){
-    const { query } = req.body; //query = { "query": {}, "limit": 0, "sort_by": "" }
-    console.log('query: ', query);
+    //TODO ADD { limit, sortby }
+    const { query } = req.body; //query = {"field": "value||expression"}
     if(query == null || query == ""){
         return res.status(500).json({
             search: false, result: "Cannot process empty!"
         });
     }
-    //TODO limit & sort_by
-    const _query = query["query"];
-    console.log('_query: ', _query);
-    game_item.find({ ..._query }, function(err, result){
-        if (err){ 
-            res.status(500).json({ search: false, result: "Please contact support. Error information.", error: err })
-        }else{
-            res.status(200).json({ search: true, result: result });
-        }
-    }); 
+    try {
+        var _query = JSON.parse(query);
+        console.log('Parsed query: ', _query);
+        game_item.find({ ..._query }, function(err, result){
+            if (err){ 
+                res.status(500).json({ search: false, result: "Please contact support. Error information.", error: err })
+            }else{
+                res.status(200).json({ search: true, result: result });
+            }
+        });
+    } catch (error) {
+        res.status(500).json({
+            search: false, result: "Error trying to parse data. Error info.", error: error,
+        })
+    } 
 });
 
 module.exports = router;
