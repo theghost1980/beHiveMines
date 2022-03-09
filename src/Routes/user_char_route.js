6e4ea5d8eeb1b3d34047eccd0893e48f8a09643b
Saturno Mangieri <saturnob612@gmail.com>
2022-03-09 12:20:08 -0400

    fixes

--- src/Routes/user_char_route.js
+++ src/Routes/user_char_route.js
@@ -41,7 +41,7 @@ router.post('/update_char',function(req,res){
             return res.status(500).json({ update_char: false, result: "Empty Record!"});
         }else{
             console.log(req.body);
-            user_char.findOneAndUpdate({ username: username, char_id: char_id }, req.body, { returnNewDocument: true }, function(err, updated){
+            user_char.findOneAndUpdate({ username: username, char_id: char_id }, req.body, { returnOriginal: false }, function(err, updated){
                 if(err){
                     res.status(500).json({
                         update_char: false, result: "Error processing the update. Please contact support. Error info.", error: err,
