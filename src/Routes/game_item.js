b52b7ee1e2d8a3d71791830c3ce58ac3a6554ce7
Saturno Mangieri <saturnob612@gmail.com>
2022-03-09 10:02:55 -0400

    fixes on how to send data on a post, usin url/enconded from godot

--- src/Routes/game_item.js
+++ src/Routes/game_item.js
@@ -11,27 +11,19 @@ router.use(bodyParser.json());
 router.post('/search', function(req,res){
     //TODO ADD { limit, sortby }
     const { query } = req.body; //query = {"field": "value||expression"}
-    console.log('Raw query: ', query);
+    console.log('query: ', query);
     if(query == null || query == ""){
         return res.status(500).json({
             search: false, result: "Cannot process empty!"
         });
     }
-    try {
-        var _query = JSON.parse(query);
-        console.log('Parsed query: ', _query);
-        game_item.find({ ..._query }, function(err, result){
-            if (err){ 
-                res.status(500).json({ search: false, result: "Please contact support. Error information.", error: err })
-            }else{
-                res.status(200).json({ search: true, result: result });
-            }
-        });
-    } catch (error) {
-        res.status(500).json({
-            search: false, result: "Error trying to parse data. Error info.", error: error,
-        })
-    } 
+    game_item.find({ ..._query }, function(err, result){
+        if (err){ 
+            res.status(500).json({ search: false, result: "Please contact support. Error information.", error: err })
+        }else{
+            res.status(200).json({ search: true, result: result });
+        }
+    });
 });
 
 module.exports = router;
